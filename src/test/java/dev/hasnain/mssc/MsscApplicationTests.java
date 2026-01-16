e3e7c9de01de2b1c937d834e7700e5d25d122031
Hasnain Memon <hasnainmemon.018@gmail.com>
2026-01-16 22:01:14 +0500

    Initial commit

---
+++ src/test/java/dev/hasnain/mssc/MsscApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hasnain.mssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
